e61ba853381d4dabbedb6c5be60baac3755b80f8
michiruuun <ayaka.k0209@gmail.com>
2020-01-16 01:05:20 +0900

    01/16 commit skil & photo update

--- main.js
+++ main.js
@@ -10,24 +10,34 @@ let app = new Vue({
       {value: 'Photo', link: '#photo'}
     ],
     languages: [
-      {value: 'VB.NET'},
-      {value: 'Java'},
-      {value: 'C#'},
-      {value: 'VB6'},
-      {value: 'VBA'},
-      {value: 'ASP.NET'},
-      {value: 'HTML'},
-      {value: 'CSS'}
+      {value: 'VB.NET', years: '約3.5年', frontFlg: true},
+      {value: 'Java', years: '約半年', frontFlg: true},
+      {value: 'C#', years: '約半年', frontFlg: true},
+      {value: 'VB6', years: '約半年', frontFlg: true},
+      {value: 'VBA', years: '約半年', frontFlg: true},
+      {value: 'ASP.NET', years: '約半年', frontFlg: true},
+      {value: 'HTML', years: '約半年', frontFlg: true},
+      {value: 'CSS', years: '約半年', frontFlg: true}
     ],
     otherSkills: [
-      {value: 'SQL Server 2012'},
-      {value: 'Management Studio'},
-      {value: 'Visual Studio 2008'},
-      {value: 'Visual Studio 2010'},
-      {value: 'Eclipce'},
-      {value: 'SVN'},
-      {value: 'CVS'},
-      {value: 'Redmine'}
+      {value: 'SQL Server 2012', years: '約3.5年', frontFlg: true},
+      {value: 'Management Studio', years: '約3.5年', frontFlg: true},
+      {value: 'Visual Studio 2008', years: '約3.5年', frontFlg: true},
+      {value: 'Visual Studio 2010', years: '約半年', frontFlg: true},
+      {value: 'Eclipce', years: '約半年', frontFlg: true},
+      {value: 'SVN', years: '約3.5年', frontFlg: true},
+      {value: 'CVS', years: '約半年', frontFlg: true},
+      {value: 'Redmine', years: '約3.5年', frontFlg: true}
+    ],
+    photos: [
+      {path: "./images/slide1.jpg", value: "スライド1"},
+      {path: "./images/slide2.jpg", value: "スライド2"},
+      {path: "./images/slide3.jpg", value: "スライド3"},
+      {path: "./images/slide4.jpg", value: "スライド4"}
     ]
+  },
+  components: {
+    'carousel': VueCarousel.Carousel,
+    'slide': VueCarousel.Slide
   }
 });
